24dccc374c10f206168a5560d403c4a26ed69d82
Jan Poonthong <janpoonthong628@gmail.com>
2021-04-09 12:35:54 +0700

    Implement rendering pixels that are on

--- main.py
+++ main.py
@@ -23,11 +23,15 @@ class Input:
 
 class Output:
     def __init__(self):
-        self.column = 64
-        self.row = 32
+        self.column = 640
+        self.row = 320
         self.display = [0] * self.column * self.row
 
-    def set_pixel(self, x, y):
+    def get_pixel(self, x, y):
+        x, y = self.check_border(x, y)
+        return self.display[x + (y * self.column)] == 1
+
+    def check_border(self, x, y):
         if x > self.column:
             x -= self.column
         elif x < 0:
@@ -38,7 +42,32 @@ class Output:
         elif y < 0:
             y += self.row
 
-        return self.display[x + (y * self.column)] != 1
+        return x, y
+
+    def set_pixel(self, x, y):
+        x, y = self.check_border(x, y)
+        self.display[x + (y * self.column)] = 1
+        return self.display[x + (y * self.column)]
 
     def clear(self):
         return self.display
+
+    def main(self):
+        self.set_pixel(5, 5)
+        self.render_pixel()
+
+    def render_pixel(self):
+        for x in range(self.column):
+            for y in range(self.row):
+                if self.get_pixel(x, y):
+                    pygame.draw.rect(SCREEN, (255, 255, 255),
+                                     (x, y, 10, 10))
+
+
+while True:
+    pygame.time.Clock().tick(15)
+    Output().main()
+    for event in pygame.event.get():
+        if event.type == pygame.QUIT:
+            sys.exit()
+    pygame.display.update()
