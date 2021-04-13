6435a612a29fb967eac8ac6ebd1d636155b77c18
Jan Poonthong <janpoonthong628@gmail.com>
2021-04-13 10:03:03 +0700

    Reverse dict

--- keyboard.py
+++ keyboard.py
@@ -4,22 +4,22 @@ import pygame
 class Keyboard:
     def __init__(self):
         self.key_map = {
-            0x1: pygame.K_1,  # 1
-            0x2: pygame.K_2,  # 2
-            0x3: pygame.K_3,  # 3
-            0xc: pygame.K_4,  # 4
-            0x4: pygame.K_q,  # Q
-            0x5: pygame.K_w,  # W
-            0x6: pygame.K_e,  # E
-            0xD: pygame.K_r,  # R
-            0x7: pygame.K_a,  # A
-            0x8: pygame.K_s,  # S
-            0x9: pygame.K_d,  # D
-            0xE: pygame.K_f,  # F
-            0xA: pygame.K_z,  # Z
-            0x0: pygame.K_x,  # X
-            0xB: pygame.K_c,  # C
-            0xF: pygame.K_v  # V
+            pygame.K_1: 0x1,  # 1
+            pygame.K_2: 0x2,  # 2
+            pygame.K_3: 0x3,  # 3
+            pygame.K_4: 0xc,  # 4
+            pygame.K_q: 0x4,  # Q
+            pygame.K_w: 0x5,  # W
+            pygame.K_e: 0x6,  # E
+            pygame.K_r: 0xD,  # R
+            pygame.K_a: 0x7,  # A
+            pygame.K_s: 0x8,  # S
+            pygame.K_d: 0x9,  # D
+            pygame.K_f: 0xE,  # F
+            pygame.K_z: 0xA,  # Z
+            pygame.K_x: 0x0,  # X
+            pygame.K_c: 0xB,  # C
+            pygame.K_v: 0xF   # V
         }
         self.key_pressed = pygame.key.get_pressed()
         self.on_next_key_press = None
