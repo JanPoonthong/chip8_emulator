4d392750c46a830ded76011c86759cd9e93511ca
Jan Poonthong <janpoonthong628@gmail.com>
2021-07-19 21:03:16 +0700

    Implement "Rest" button in GUI

--- renderer.py
+++ renderer.py
@@ -62,25 +62,42 @@ class Renderer:
                     (x, y, self.scale_w, self.scale_h),
                 )
         self.menu_bar()
+        self.rest_button()
 
     def menu_bar(self):
-        color = 255, 255, 255
+        self.color = 255, 255, 255
         self.color_dark = 100, 100, 100
         self.color_light = 170, 170, 170
-        font = pygame.font.SysFont("Corbel", 20)
-        text = font.render("File", True, color)
+        self.font = pygame.font.SysFont("Corbel", 20)
+        text = self.font.render("File", True, self.color)
         mouse = pygame.mouse.get_pos()
-        cursor_on_file = (
+        self.cursor_on_file = (
             13 / 2 <= mouse[0] <= 13 / 2 + 27
             and 3 / 2 <= mouse[1] <= 3 / 2 + 17
         )
-        if cursor_on_file:
+        if self.cursor_on_file:
             pygame.draw.rect(self.screen, self.color_dark, (13, 3, 27, 17))
         else:
             pygame.draw.rect(self.screen, self.color_light, (13, 3, 27, 17))
         self.screen.blit(text, (15, 5))
 
     def loading_new_rom(self):
-        if pygame.mouse.get_pressed()[0]:
+        if pygame.mouse.get_pressed()[0] and self.cursor_on_file:
             game_rom = file_explorer()
             return game_rom
+
+    def rest_button(self):
+        mouse = pygame.mouse.get_pos()
+        text = self.font.render("Rest", True, self.color)
+        cursor_on_file = (
+            98 <= mouse[0] <= 98 + 32 and 3 / 2 <= mouse[1] <= 3 / 2 + 17
+        )
+        if cursor_on_file:
+            pygame.draw.rect(self.screen, self.color_dark, (98, 3, 32, 17))
+            if pygame.mouse.get_pressed()[0]:
+                cpu = Cpu(Renderer(), Keyboard())
+                cpu.reset()
+                self.clear()
+        else:
+            pygame.draw.rect(self.screen, self.color_light, (98, 3, 32, 17))
+        self.screen.blit(text, (100, 5))
