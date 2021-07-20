e5bcedb0fcb2bf9a577e7270b75084fac9f6e1b9
Jan Poonthong <janpoonthong628@gmail.com>
2021-07-20 09:34:58 +0700

    Remembering which rom is loaded

--- chip8.py
+++ chip8.py
@@ -11,18 +11,23 @@ from menu import file_explorer
 def main(game_rom):
     cpu.load_sprites_into_memory()
     cpu.load_rom(f"{game_rom}", 0x200)
+    first_rom = game_rom
     while True:
         game_rom_again = renderer.loading_new_rom()
+        reset_game = renderer.reset_rom()
+
         if game_rom_again is not None:
             cpu.load_rom(f"{game_rom_again}", 0x200)
-        reset_game = renderer.reset_rom()
+            first_rom = None
+
         if reset_game is True:
             cpu.reset()
             cpu.load_sprites_into_memory()
-            if game_rom_again is not None:
+            if first_rom is None:
                 cpu.load_rom(f"{game_rom_again}", 0x200)
             else:
                 cpu.load_rom(f"{game_rom}", 0x200)
+
         cpu.cycle()
         pygame_screen()
 
